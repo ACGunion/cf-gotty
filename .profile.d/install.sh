157594f8f11e98ff20913355f8596ddde721fc72
ACGunion <kung.blog@gmail.com>
2016-02-28 11:18:01 +0800

    新版本
    修改为新版本

--- .profile.d/install.sh
+++ .profile.d/install.sh
@@ -3,9 +3,9 @@ sleep 2
 echo -n "Installing gotty..."
 curl -s -L -o "$HOME/linux_amd64.tar.gz" "https://github.com/yudai/gotty/releases/download/v0.0.11/linux_amd64.tar.gz"
 tar zxf "$HOME/linux_amd64.tar.gz"
-mv gotty app/
+mv gotty app/gotty/
 rm "$HOME/linux_amd64.tar.gz"
-ls -la 
-ls -la "$HOME/"
-chmod 755 "$HOME/gotty"
+ls -la
+ls -la "$HOME/gotty/"
+chmod 755 "$HOME/gotty/gotty"
 echo " done".
